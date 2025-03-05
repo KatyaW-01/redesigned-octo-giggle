fda6a8290da7c5559b9b1af28994de2517a89185
Katya Weicht <katya.weicht@gmail.com>
2025-03-05 10:04:43 -0700

    Write method for vendor names

--- spec/market_spec.rb
+++ spec/market_spec.rb
@@ -56,7 +56,7 @@ RSpec.describe Market do
             @market.add_vendor(vendor2)  
             @market.add_vendor(vendor3)
 
-            expect(@market.vendor_names).to eq["Rocky Mountain Fresh","Ba-Nom-a-Nom","Palisade Peach Shack"]
+            expect(@market.vendor_names).to eq(["Rocky Mountain Fresh","Ba-Nom-a-Nom","Palisade Peach Shack"])
 
         end
     end
