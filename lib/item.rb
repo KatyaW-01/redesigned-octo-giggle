61dda657e351304d470e740a840db2bfa011dfd1
Katya Weicht <katya.weicht@gmail.com>
2025-03-05 09:12:08 -0700

    Write tests for item class

--- lib/item.rb
+++ lib/item.rb
@@ -1,3 +1,5 @@
 class Item
-    
+    def initialize(item_data)
+        
+    end
 end
--- spec/item_spec.rb
+++ spec/item_spec.rb
@@ -1,5 +1,24 @@
-require './lib/iten'
+require './lib/item'
 
 RSpec.describe Item do
-    
+    describe "#initialize" do
+        it 'exists' do
+            item1 = Item.new({name: 'Peach', price: "$0.75"})
+            item2 = Item.new({name: 'Tomato', price: '$0.50'})
+
+            expect(item1).to be_a(Item)
+            expect(item2).to be_a(Item)
+        end
+    end
+    describe "#attributes" do
+        it 'has a name' do
+            item1 = Item.new({name: 'Peach', price: "$0.75"})
+            expect(item1.name).to eq('Peach')
+        end
+        it 'has a price' do
+            item2 = Item.new({name: 'Tomato', price: '$0.50'})
+            expect(item2.price).to eq('$0.50')
+        end
+    end
+   
 end
